af8e433c5d82cac9df75039466fde9af88edd77c
Mathivanan Marimuthu <35862356+jackmathi@users.noreply.github.com>
2018-07-03 16:02:26 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * demo Product functions and definitions
- *yogisukumar.mails@gmail.com
+ *
  * Set up the theme and provides some helper functions, which are used in the
  * theme as custom template tags. Others are attached to action and filter
  * hooks in WordPress to change core functionality.
